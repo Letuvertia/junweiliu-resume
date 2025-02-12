1a0d0d0d3bc54a195a92a51c6b993e9d5ca5f279
Jun-Wei Liu <1026oscar1@gmail.com>
2025-02-12 15:36:07 +0800

    Update resume to 2025 version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t junweiliu-resume .
-docker run --rm -i -v "$PWD":/data junweliu-resume pdflatex junweiliu-resume.tex
+docker run --rm -i -v "$PWD":/data junweiliu-resume pdflatex junweiliu-resume.tex
